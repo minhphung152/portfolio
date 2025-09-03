922b26dc406a5813a5d75f56912ca5fb0d372aac
minhphung152 <98346569+minhphung152@users.noreply.github.com>
2025-09-03 15:56:57 -0700

    Added About Me button on Home page

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -22,6 +22,12 @@ export const Home = () => {
                             href="#projects" 
                             className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
                         >
+                            About Me
+                        </a>
+                        <a
+                            href="#about"
+                            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 245, 0.4)]"
+                        >
                             View Projects
                         </a>
                         <a 
