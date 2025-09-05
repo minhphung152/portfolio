8c4df029eb2235678b7d61da12f6474887d1adf0
minhphung152 <98346569+minhphung152@users.noreply.github.com>
2025-09-04 20:35:28 -0700

    Fixed mismatched links

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -19,13 +19,13 @@ export const Home = () => {
 
                     <div className="flex justify-center space-x-4">
                         <a 
-                            href="#projects" 
+                            href="#about" 
                             className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
                         >
                             About Me
                         </a>
                         <a
-                            href="#about"
+                            href="#projects"
                             className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 245, 0.4)]"
                         >
                             View Projects
